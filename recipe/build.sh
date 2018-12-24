bba886d254c6c0c3be76562b056539ce7be80c6c
Ray Donnelly <mingw.android@gmail.com>
2018-12-24 17:42:58 +0000

    No not define NDEBUG, it breaks the gnutls tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,8 @@ fi
 # export CFLAGS="-march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-all -fno-plt -Og -g -Wall -Wextra -fvar-tracking-assignments -ffunction-sections -pipe -I${PREFIX}/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix"
 # export CPPFLAGS="-D_DEBUG -D_FORTIFY_SOURCE=2 -Og"
 
-# export CPPFLAGS="${CPPFLAGS//-DNDEBUG/}"
+export CPPFLAGS="${CPPFLAGS//-DNDEBUG/}"
+
 # export CFLAGS="${CFLAGS} --save-temps"
 
 # export LDFLAGS="-Wl,-O2 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now -Wl,--disable-new-dtags -Wl,--gc-sections -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
